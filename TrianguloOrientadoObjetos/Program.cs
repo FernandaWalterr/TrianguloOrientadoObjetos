2b22073ed4454c9b4dc12a59b2ef12cde3d03786
feWalter <taimisefernanda@gmail.com>
2023-04-06 15:14:57 -0300

    Adicionar arquivos de projeto.

---
+++ TrianguloOrientadoObjetos/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace TrianguloOrientadoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.DefinirLados(x, y, z);
+
+            if (triangulo.EhTriangulo())
+            {
+                if (triangulo.EhEquilatero())
+                {
+                    Console.WriteLine("É um triângulo equilátero!");
+                }
+                else if (triangulo.EhIsosceles())
+                {
+                    Console.WriteLine("É um triângulo isósceles!");
+                }
+                else
+                {
+                    Console.WriteLine("É um triângulo escaleno!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é possível formar um triângulo com esses valores!");
+            }
+        }
+    }
+}
---
+++ TrianguloOrientadoObjetos/s.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoObjetos
+{
+    class Triangulo
+    {
+        private double x, y, z;
+
+        public void DefinirLados(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public bool EhTriangulo()
+        {
+            return (x + y > z && x + z > y && y + z > x);
+        }
+
+        public bool EhEquilatero()
+        {
+            return (x == y && x == z);
+        }
+
+        public bool EhIsosceles()
+        {
+            return (x == y || x == z || y == z);
+        }
+    }
+}
